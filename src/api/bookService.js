dda25277f9dd50b470e9be23548c9ff5c0f828df
euihyeong <kkh60245@gmail.com>
2025-05-30 17:24:28 +0900

    fix: axio 수정
    backend와 api 연동할 수 있도록 수정

--- src/api/bookService.js
+++ src/api/bookService.js
@@ -3,10 +3,10 @@ import axiosInstance from "./axiosInstance";
 //도서 목록 조회
 export const fetchBooks = async (title = '') => {
     try {
-        const response = await axiosInstance.get('/books', {
-            params: { title }
+        const response = await axiosInstance.get(`/books`, {
+            params: {title}
         });
-        return response.data.data;
+        return response.data;
     } catch (error) {
         handleError(error);
     }
@@ -16,7 +16,7 @@ export const fetchBooks = async (title = '') => {
 //도서 상세 조회
 export const fetchBookById = async (id) => {
     try {
-        const response = await axiosInstance.get('/books/${id}');
+        const response = await axiosInstance.get(`/books/${id}`);
         return response.data.data;
     } catch (error) {
         handleError(error);
@@ -26,7 +26,7 @@ export const fetchBookById = async (id) => {
 //도서 등록
 export const createBook = async (bookData) => {
     try {
-        const response = await axiosInstance.post('/books', bookData);
+        const response = await axiosInstance.post(`/books`, bookData);
         return response.data.data;
     } catch (error) {
         handleError(error);
@@ -36,7 +36,7 @@ export const createBook = async (bookData) => {
 //도서 수정
 export const updateBook = async (id, bookData) => {
     try {
-        const response = await axiosInstance.put('/books/${id}', bookData);
+        const response = await axiosInstance.put(`/books/${id}`, bookData);
         return response.data.data;
     } catch (error) {
         handleError(error);
@@ -46,7 +46,7 @@ export const updateBook = async (id, bookData) => {
 //도서 삭제
 export const deleteBook = async (id) => {
     try {
-        const reponse = await axiosInstance.delete('/books/${id}');
+        const reponse = await axiosInstance.delete(`/books/${id}`);
         return reponse.data.message;
     } catch (error) {
         handleError(error);
